2eab8e73fd9a45c9f2a2bfe79734ade6605894f5
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-02-19 20:46:26 +0200

    intial

---
+++ app/src/main/java/com/example/fooddelivery/ProfileDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fooddelivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_details)
+    }
+}
