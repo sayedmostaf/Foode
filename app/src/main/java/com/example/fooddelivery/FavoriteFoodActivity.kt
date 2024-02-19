c524fb5eac53977002d577afb063fdfb76cd955f
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-02-19 15:06:13 +0200

    intial

---
+++ app/src/main/java/com/example/fooddelivery/FavoriteFoodActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fooddelivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavoriteFoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite_food)
+    }
+}
---
+++ app/src/main/java/com/example/fooddelivery/SelectPaymentMethodActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fooddelivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectPaymentMethodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_payment_method)
+    }
+}
